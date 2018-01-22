5cc29aa4eee5fce3a1a178af7ed85b07c738bd66
Imran <imran-ib@outlook.com>
2018-01-22 22:30:15 +0300

    navbar partially finished

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var simpleVars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var imports = require('postcss-import');
+var mixins = require("postcss-mixins");
+var hexrbga = require('postcss-hexrgba');
+
+
+gulp.task('styles' , function(){
+	return gulp.src("./app/assets/styles/styles.css")
+		.pipe(postcss([
+				imports,
+				mixins,
+				simpleVars,
+				nested,
+				hexrbga,
+				autoprefixer
+
+			]))
+		.on("error" , function(errorInfo){
+			console.log(errorInfo.toString());
+			this.emit("end");
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+
+
+});
